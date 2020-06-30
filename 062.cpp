464fd1f88892983f3ad821a49f3aad84062190c4
Charles An <mucollabo@gmail.com>
2020-06-30 16:32:22 +0900

    cpp200 study until 070

--- 062.cpp
+++ 062.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main()
 {
     string sentence = "i like coding";
+    string find_str = "coding";
+    string replace_str = "history";
 
-    sentence.erase(remove(sentence.begin(), sentence.end(), ' '), sentence.end());
+    sentence.replace(sentence.find(find_str), find_str.length(), replace_str);
 
     cout << sentence << endl;
 
